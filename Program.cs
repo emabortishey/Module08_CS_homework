b93534835461c7821c74e1569adbe5ac51aa228a
fartb <fartbreather228@gmail.com>
2025-05-29 01:01:42 +0300

    2 заданиеЖ создан фундамет трёх классов

--- Program.cs
+++ Program.cs
@@ -230,3 +230,53 @@ public abstract class Car
 
     public abstract void Ride();
 }
+
+/*
+
+Программа «Карточная игра!»
+Создать модель карточной игры.
+Требования:
+1. Класс Game формирует и обеспечивает:
+1.1.1. Список игроков (минимум 2);
+1.1.2. Колоду карт (36 карт);
+1.1.3. Перетасовку карт (случайным образом);
+1.1.4. Раздачу карт игрокам (равными частями каждому
+игроку);
+1.1.5. Игровой процесс. Принцип: Игроки кладут по
+одной карте. У кого карта больше, то тот игрок
+забирает все карты и кладет их в конец своей
+колоды. Упрощение: при совпадении карт забирает первый игрок, шестерка не забирает туза.
+Выигрывает игрок, который забрал все карты.
+2. Класс Player (набор имеющихся карт, вывод имеющихся карт).
+3. Класс Karta (масть и тип карты (6–10, валет, дама,
+король, туз).
+
+*/
+
+public class Card
+{
+    public string _suit { get; set; }
+    public string _type { get; set; }
+}
+
+public class Player
+{
+    public string _nick { get; set; }
+    List<Card> _cards { get; set; }
+
+    public void PrintCards()
+    {
+        WriteLine($"{_nick}'s card collection:\n\n");
+
+        foreach (var card in _cards)
+        {
+            Write($"{card} ");
+        }
+    }
+}
+
+public class Game
+{
+    List<Player> _players { get; set; }
+    List<Card> _deck {  get; set; }
+}
